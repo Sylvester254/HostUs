e036ad8e3c46d181834b3f8e05ea142b1077dfa3
Sylvester254 <slynganga59@gmail.com>
2023-01-10 00:14:48 +0300

    add hostel and update profile,with some errors still

--- Web_flask/app.py
+++ Web_flask/app.py
@@ -24,40 +24,18 @@ mysql = MySQL(app)
 def index():
     """ Display home page and all the hostels"""
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT h.*, o.Owner_name, o.Email, o.Phone_no FROM Hostel h JOIN User o ON h.Owner_id = o.Owner_id')
+    cursor.execute('SELECT h.*, o.Owner_name FROM Hostel h JOIN User o ON h.Owner_id = o.Owner_id')
     hostel = cursor.fetchall()
     return render_template("Index.html", hostel=hostel)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    msg = ''
-    if request.method == 'POST' and 'Username' in request.form and 'Password' in request.form:
-        Username = request.form['Username']
-        Password = request.form['Password']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            ''' SELECT * FROM User WHERE Username = %s AND Password = %s''', (Username, Password))
-        account = cursor.fetchone()
-        if account:
-            session['loggedin'] = True
-            session['Owner_id'] = account['Owner_id']
-            session['Username'] = account['Username']
-            msg = 'Logged in successfully !'
-            return render_template('Profile.html', msg=msg, account=account)
-        else:
-            msg = 'Incorrect username / password !'
-        mysql.connection.commit()
-        cursor.close()
-    return render_template('Login.html', msg=msg)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('loggedin', None)
-    session.pop('id', None)
-    session.pop('username', None)
-    return redirect(url_for('login'))
+@app.route('/hostels/<int:Hostel_id>')
+def hostels(Hostel_id):
+    """ Display hostel page for the requested hostel at home page"""
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT h.*, o.Owner_name, o.Email, o.Phone_no FROM Hostel h JOIN User o ON h.Owner_id = o.Owner_id WHERE h.Hostel_id = %s', (Hostel_id,))
+    hostel = cursor.fetchone()
+    return render_template("Hostel.html", hostel=hostel)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -89,9 +67,74 @@ def register():
     return render_template('Signup.html', msg=msg)
 
 
-@app.route('/manage', methods=['GET', 'POST', 'DELETE'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'Username' in request.form and 'Password' in request.form:
+        Username = request.form['Username']
+        Password = request.form['Password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            ''' SELECT * FROM User WHERE Username = %s AND Password = %s''', (Username, Password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['Owner_id'] = account['Owner_id']
+            session['Username'] = account['Username']
+            msg = 'Logged in successfully !'
+            return redirect(url_for('profile', id=account['Owner_id']))
+        
+        else:
+            msg = 'Incorrect username / password !'
+        mysql.connection.commit()
+        cursor.close()
+    return render_template('Login.html', msg=msg)
+
+@app.route("/profile/<int:id>")
+def profile(id):
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(''' SELECT * FROM User WHERE Owner_id = %s''', (id,))
+        account = cursor.fetchone()
+        return render_template("Profile.html", account=account)
+    return redirect(url_for('login'))
+
+
+@app.route('/profile/<int:id>/add', methods=['GET','POST'])
+def add(id):
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(''' SELECT * FROM User WHERE Owner_id = %s''', (id,))
+        account = cursor.fetchone()
+        msg = ''
+        # Owner_id=session['Owner_id']
+        if request.method == 'POST' and 'Hostel_name' in request.form and 'Features' in request.form and 'Location' in request.form and 'Campus' in request.form and 'Occupants' in request.form and 'Rooms' in request.form and 'WiFi' in request.form and 'Rent' in request.form:
+            Hostel_name = request.form['Hostel_name']
+            Features = request.form['Features']
+            Location = request.form['Location']
+            Campus = request.form['Campus']
+            Occupants = request.form['Occupants']
+            Rooms = request.form['Rooms']
+            WiFi = request.form['WiFi']
+            Rent = request.form['Rent']
+        
+            Owner_id = account['Owner_id']
+        
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('''INSERT INTO User VALUES (NULL, %s, %s, %s, %s, %s, Now(), Now(), %s, %s, %s, %s, NULL)''',
+                            (Owner_id, Hostel_name, Features, Location, Campus, Occupants, Rooms, WiFi, Rent, (session['id'])))
+            mysql.connection.commit()
+            cursor.close()
+            msg = 'Hostel successfully added !'
+        elif request.method == 'POST':
+            msg = 'Please fill out the form !'
+        return render_template('Add.html', msg=msg, account=account)
+
+
+@app.route('/manage', methods=['GET', 'PUT', 'DELETE'])
 def manage():
     msg = ''
+    Owner_id=session['Owner_id']
     if request.method == 'POST' and 'Hostel_name' in request.form and 'Features' in request.form and 'Location' in request.form and 'Campus' in request.form and 'Occupants' in request.form and 'Rooms' in request.form and 'WiFi' in request.form and 'Rent' in request.form:
         Hostel_name = request.form['Hostel_name']
         Features = request.form['Features']
@@ -102,8 +145,8 @@ def manage():
         WiFi = request.form['WiFi']
         Rent = request.form['Rent']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('''INSERT INTO User VALUES (NULL, NULL, %s, %s, %s, %s, Now(), Now(), %s, %s, %s, %s, NULL)''',
-                           (Hostel_name, Features, Location, Campus, Occupants, Rooms, WiFi, Rent))
+        cursor.execute('''INSERT INTO User VALUES (NULL, %s, %s, %s, %s, %s, Now(), Now(), %s, %s, %s, %s, NULL)''',
+                           (Owner_id, Hostel_name, Features, Location, Campus, Occupants, Rooms, WiFi, Rent))
         mysql.connection.commit()
         cursor.close()
         msg = 'Hostel successfully added !'
@@ -112,47 +155,42 @@ def manage():
     return render_template('Manage.html', msg=msg)
 
 
-
-
-@app.route("/profile")
-def profile():
+@app.route("/profile/<int:id>/update", methods=['GET', 'POST'])
+def update(id):
+    msg = ''
     if 'loggedin' in session:
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(''' SELECT * FROM User WHERE Owner_id = %s''', (session['Owner_id']))
+        cursor.execute(
+                'SELECT * FROM User WHERE Owner_id = %s', (id,))
         account = cursor.fetchone()
-        return render_template("Profile.html", account=account)
+        
+        Owner_name = request.form['Owner_name']
+        Email = request.form['Email']
+        Phone_no = request.form['Phone_no']
+        Username = request.form['Username']
+        Password = request.form['Password']
+            
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', Email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', Username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            cursor.execute('UPDATE User SET  Owner_name =%s, Email =%s, Phone_no =%s, Username =%s, Password =%s', (
+                    Owner_name, Email, Phone_no, Username, Password))
+            mysql.connection.commit()
+            cursor.close()
+            msg = 'You have successfully updated your details!'
+    elif request.method == 'GET':
+        msg = 'Please fill out the form !'
+        return render_template("Update.html", msg=msg, account=account)
     return redirect(url_for('login'))
 
 
-@app.route("/update", methods=['GET', 'POST'])
-def update():
-    msg = ''
-    if 'loggedin' in session:
-        if request.method == 'POST':
-            Owner_name = request.form['Owner_name']
-            Email = request.form['Email']
-            Phone_no = request.form['Phone_no']
-            Username = request.form['Username']
-            Password = request.form['Password']
-            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute(
-                'SELECT * FROM User WHERE username = %s', (Username))
-            account = cursor.fetchone()
-            if account:
-                msg = 'Account already exists !'
-            elif not re.match(r'[^@]+@[^@]+\.[^@]+', Email):
-                msg = 'Invalid email address !'
-            elif not re.match(r'[A-Za-z0-9]+', Username):
-                msg = 'name must contain only characters and numbers !'
-            else:
-                cursor.execute('UPDATE User SET  Owner_name =%s, Email =%s, Phone_no =%s, Username =%s, Password =%s', (
-                    Owner_name, Email, Phone_no, Username, Password, (session['id'])))
-                mysql.connection.commit()
-                cursor.close()
-                msg = 'You have successfully updated !'
-        elif request.method == 'POST':
-            msg = 'Please fill out the form !'
-        return render_template("Update.html", msg=msg)
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
     return redirect(url_for('login'))
 
 
@@ -166,15 +204,6 @@ def about():
 def contact():
     """ Display Contact page"""
     return render_template("Contact.html")
-
-
-@app.route('/hostels/<int:Hostel_id>')
-def hostels(Hostel_id):
-    """ Display hostel page for the requested hostel at home page"""
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT h.*, o.Owner_name, o.Email, o.Phone_no FROM Hostel h JOIN User o ON h.Owner_id = o.Owner_id WHERE h.Hostel_id = %s', (Hostel_id,))
-    hostel = cursor.fetchone()
-    return render_template("Hostel.html", hostel=hostel)
 
 
 if __name__ == "__main__":
